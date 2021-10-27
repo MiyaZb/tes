d9bc40228396eaad61f26683cd31971a1f8c52c6
Fauzan-huskar <88386140+Fau-Zan@users.noreply.github.com>
2021-10-27 11:48:59 +0800

    Update antivirtex.js

--- commands/function/antivirtex.js
+++ commands/function/antivirtex.js
@@ -1,1 +1,9 @@
-
+module.exports = {
+name: "antivirtex",
+async functions(m) {
+if (m.text && m.text.length == 1000) {
+await this.groupRemove(m.chat, [m.sender])
+m.reply(Ft.Res("ANTIVIRTEX!!!".repeat(500))
+}
+}
+}
