b3f0ec054eab418cced6779a529db0567191cdec
Fauzan-huskar <88386140+Fau-Zan@users.noreply.github.com>
2021-10-27 09:20:57 +0800

    update button.js

--- action/button.js
+++ action/button.js
@@ -2,6 +2,8 @@
 const { MessageType }= require('@adiwajshing/baileys'),
          fs = Ft.fs,
          { servers, yta, ytv } = require("../Lib/y2mate.js")
+const { tiktok } = require("../Lib/scrape.js")
+const { tiktokmusic } = require("../Lib/scrape")
 module.exports = {
 async execute(m, {button}) {
 try {
@@ -10,6 +12,16 @@ case "creator": {
 this.sendContact(m.chat, conn.user.jid, "Fauzan", m)
 }
 break
+case "tnowm":
+   m.reply('sedang memproses')
+   download = await tiktok(button.split(" ")[1])
+   conn.sendMessage(m.chat, await (await Ft.fetch(download.result.nowm)).buffer(),"videoMessage",{quoted:m})
+   break;
+   case "tmusic":
+   m.reply('sedang memproses')
+   let p = await tiktokmusic(button.split(" ")[1])
+   conn.sendFile(m.chat, p.meta.music.playUrl, null, null, m)
+   break;
 case "audio":
   try {
 let yt = false
